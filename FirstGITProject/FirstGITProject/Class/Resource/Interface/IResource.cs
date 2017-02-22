dd865bb7c4a6e7afacafdca28f0725ce3cc8adde
GSINET\marcelod <jellomarcelo@gmail.com>
2017-02-22 16:57:16 +0800

    Branch changes

---
+++ FirstGITProject/FirstGITProject/Class/Resource/Interface/IResource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstGITProject.Class.Resource.Interface
+{
+    public class IResource
+    {
+        int ID { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string MiddleInitial { get; set; }
+        DateTime DateOfBirth { get; set; }
+
+
+    }
+}
